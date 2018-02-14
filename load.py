ac2d57867f6cc8ad18e7291a27ae77682b4bb3e8
martyn pratt <martyni@automationlogic.com>
2018-02-14 16:12:50 +0000

    broke out sprites from main and started creating loading function

--- load.py
+++ load.py
@@ -1,2 +1,40 @@
 import os
+import yaml
+from pprint import pprint
+from controls import log
 
+assets =[{i[0] :[i[1], i[2]]} for i in os.walk('assets')] 
+assets ={i[0] :[i[1], i[2]] for i in os.walk('assets')}
+levels = {i: {} for i in assets['assets'][0]}
+
+class Base(object):
+    def __init__(self, level, files, directories):
+        self.level = level
+        self.__name__ = self.level
+        self.files = files
+        self.directories = directories
+        self.images = []
+        self.settings = {}
+        self.parse_files()
+
+    def get_file(self, item):
+        return 'assets/{level}/{item}'.format(
+                level=self.level,
+                item=item
+                )
+
+    def parse_files(self):
+        for filename in files:
+            if '.jpg' in filename or 'png' in filename:
+                self.images.append(self.get_file(filename))
+            if '.yml' in filename or '.yaml' in filename:
+                with open(self.get_file(filename), 'r') as cfg:
+                    config = yaml.load(cfg.read())
+                    self.settings.update(config)
+                pprint(config)
+
+for level in levels:
+    level_key = '/'.join(['assets', level] )
+    dirs = assets[level_key][0]
+    files = assets[level_key][1]
+    levels[level] = Base(level, files, dirs)
--- main.py
+++ main.py
@@ -1,59 +1,6 @@
 import controls
 from controls import pygame, log
-
-class base_sprite(pygame.sprite.Sprite):
-    def __init__(
-            self,
-            width,
-            height,
-            states= {"default": [pygame.image.load("test.png").convert_alpha()]},
-            i=0,
-            j=0,
-            ):
-        self.state="default"
-        self.width = width
-        self.height = height
-        self.states = states
-        self.step = 0
-        self.i = i
-        self.j = j
-        self.image = pygame.transform.scale(
-                self.states[self.state][self.step],
-                (self.width, self.height)
-                ).copy()
-
-    def update(self):
-        controls.screen.blit(
-                pygame.transform.scale(
-                   self.states[self.state][self.step], 
-                   (self.width, self.height)
-                   ),
-                   (self.i + controls.x,self.j + controls.y),
-        )
-
-class player_sprite(base_sprite):
-   x = 250
-   y = 250
-   def update(self):
-        if self.step >= len(self.states[self.state]):
-           self.step = 0
-        controls.screen.blit(
-                pygame.transform.scale(
-                   self.states[self.state][self.step], 
-                   (self.width, self.height)
-                   ),
-                   (self.x, self.y),
-        )
-        self.state = "default"
-        for action in controls.action:
-            if controls.action[action]:
-                self.state = action
-                
-        self.step += 1
-        self.i = self.x - controls.x  
-        self.j = self.y - controls.y 
-
-
+from sprites import base_sprite, player_sprite
 
 background = base_sprite(500, 500)
 npc = base_sprite(50, 50, i=250, j=250)
