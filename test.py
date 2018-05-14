c26a9cd179dd47e81d164dd8a9b1f76c0a47a9c8
Martyn Pratt <martyni@automationlogic.com>
2018-05-14 15:35:22 +0100

    linting test.py

--- test.py
+++ test.py
@@ -1,16 +1,28 @@
-import pygame
-from pygame import event as ev
-from copy import deepcopy
-from pygame.locals import *
+""" Test script, mainly used to translate keys to events """
 from random import randint
+import pygame
+# pylint: disable=wildcard-import, undefined-variable, unused-wildcard-import, no-member
+# Standard pygame tricks
+from pygame.locals import *
+from pygame import event as ev
 pygame.init()
+# pylint: disable=invalid-name
+# Disagree with ALL_CAPS variables atleast in test function
+width, height = 500, 500
+screen = pygame.display.set_mode(
+    (width, height),
+    HWSURFACE | DOUBLEBUF | RESIZABLE)
 
 
-def main(background_layers=[], sprites=[]):
-    width, height = 500, 500
+# pylint: disable=dangerous-default-value
+# I also like to live dangerously
+def main(sprites=[]):
+    """ Test function """
+    # pylint: disable= global-statement
+    # Why is there a global statement that I'm not allowed to use?
     global screen
-    screen = pygame.display.set_mode(
-        (width, height), HWSURFACE | DOUBLEBUF | RESIZABLE)
+    global width
+    global height
     game = True
     while game:
         ev.pump()
@@ -24,8 +36,10 @@ def main(background_layers=[], sprites=[]):
                 width, height = event.dict['size']
                 screen = pygame.display.set_mode(
                     (width, height), HWSURFACE | DOUBLEBUF | RESIZABLE)
+                # pylint: disable=expression-not-assigned
+                # Don't want this to be assigned.
                 [sprite.resize(width, height) for sprite in sprites]
-            print(event)
+            print event
 
         screen.fill((randint(1, 255), randint(1, 255), randint(1, 255)))
         pygame.display.flip()
