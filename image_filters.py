2d9610abc1ceaff955c356acfd6e3c01cebd1f7d
Martyn Pratt <martyni@automationlogic.com>
2018-05-15 12:09:32 +0100

    linted image_filters

--- image_filters.py
+++ image_filters.py
@@ -1,12 +1,14 @@
-from PIL import Image, ImageChops
-from PIL.ImageFilter import *
+"""Custom image filters """
+from PIL import ImageChops
+from PIL.ImageFilter import BLUR
 
 
 def ghost(image, ghostyness=10, show=True):
-    a = image.filter(BLUR)
-    for i in range(ghostyness):
-        a = a.filter(BLUR)
-    final = ImageChops.lighter(a, image)
+    """ Returns ghostly looking shining image"""
+    blurred_image = image.filter(BLUR)
+    for _ in range(ghostyness):
+        blurred_image = blurred_image.filter(BLUR)
+    final = ImageChops.lighter(blurred_image, image)
     if show:
         final.show()
     return final
