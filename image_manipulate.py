42bb54bc005b9b94abe27c156bffe3747c1c1c62
Martyn Pratt <martyni@automationlogic.com>
2018-05-31 10:14:12 +0100

    linting

--- image_manipulate.py
+++ image_manipulate.py
@@ -1,11 +1,12 @@
-from PIL import Image
+"""Module for manipulating images"""
 from os import walk
+#from PIL import Image
 
-DIR = ["top", "bottom", "left", "right"]:
-IMAGES = [i for i in walk('assets/images/')][0][-1]
-JPGS = [j for j in images if 'jpg' in j]
-MASKS = [ m for m in jpgs if 'mask' in m]
+DIR = ["top", "bottom", "left", "right"]
+IMAGES = [_ for _ in walk('assets/IMAGES/')][0][-1]
+JPGS = [_ for _ in IMAGES if 'jpg' in _]
+MASKS = [_ for _ in JPGS if 'mask' in _]
 BASE_TILES = []
 #for jpg in JPGS:
 #    if jpg not in MASKS:
-#        for 
+#        for
